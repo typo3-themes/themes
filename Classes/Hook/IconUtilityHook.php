4d5d8cc519d07e78b88d13739a7e058e32c91a54
Kay Strobach <github@kay-strobach.de>
2014-02-19 15:52:01 +0100

    add files from new version

---
+++ Classes/Hook/IconUtilityHook.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KayStrobach\Themes\Hook;
+
+/**
+ * Class PageNotFoundHandlingHook
+ * @package KayStrobach\Themes\Hook
+ */
+
+class IconUtilityHook {
+	public function overrideIconOverlay($table, $row, &$status) {
+
+	}
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,42 +2,52 @@
 
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-// Allo legacy code, will be removed lateron
+/**
+ * Allow legacy code, will be removed lateron
+ */
 
-class_alias('\\KayStrobach\\Themes\\Domain\\Model\\Theme',                'Tx_Themes_Domain_Model_Theme');
-class_alias('\\KayStrobach\\Themes\\Domain\\Repository\\ThemeRepository', 'Tx_Themes_Domain_Repository_ThemeRepository');
+	class_alias('\\KayStrobach\\Themes\\Domain\\Model\\Theme',                'Tx_Themes_Domain_Model_Theme');
+	class_alias('\\KayStrobach\\Themes\\Domain\\Repository\\ThemeRepository', 'Tx_Themes_Domain_Repository_ThemeRepository');
 
 /**
  * Register hook to inject themes
  */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Tx_Themes_Domain_Repository_ThemeRepository']['init'][]
-	= 'KayStrobach\\Themes\\Hook\\ThemesDomainRepositoryThemeRepositoryInitHook->init';
-
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Tx_Themes_Domain_Repository_ThemeRepository']['init'][]
+		= 'KayStrobach\\Themes\\Hook\\ThemesDomainRepositoryThemeRepositoryInitHook->init';
 
 /**
  * Register a page not found handler, overwrites the one in the install tool
  */
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['pageUnavailable_handling'] = 'USER_FUNCTION:KayStrobach\\Themes\\Hook\\PageNotFoundHandlingHook->main';
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['pageUnavailable_handling']
+		= 'USER_FUNCTION:KayStrobach\\Themes\\Hook\\PageNotFoundHandlingHook->main';
 
 /**
  * register used hooks to inject the TS
  */
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][]
-	= 'KayStrobach\\Themes\\Hook\\T3libTstemplateIncludeStaticTypoScriptSourcesAtEndHook->main';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][]
+		= 'KayStrobach\\Themes\\Hook\\T3libTstemplateIncludeStaticTypoScriptSourcesAtEndHook->main';
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['moduleBodyPostProcess'][]
-	= 'KayStrobach\\Themes\\Hook\\TemplateModuleBodyPostProcessHook->main';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['moduleBodyPostProcess'][]
+		= 'KayStrobach\\Themes\\Hook\\TemplateModuleBodyPostProcessHook->main';
 
+/**
+ * Register XClasses to be a bit compatible to older versions
+ */
 
-if(version_compare(TYPO3_version, '6.2.0', '<')) {
-	/**
-	 * Register Xclasses the new way from 6.0
-	 * @todo test
-	 */
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Backend\\Configuration\\TsConfigParser'] = array(
-		'className' => 'KayStrobach\\Themes\\XClass\\TsConfigParser',
-	);
-}
+	if(version_compare(TYPO3_version, '6.2.0', '<')) {
+		/**
+		 * Register Xclasses the new way from 6.0
+		 * @todo test
+		 */
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Backend\\Configuration\\TsConfigParser'] = array(
+			'className' => 'KayStrobach\\Themes\\XClass\\TsConfigParser',
+		);
+	}
 
+/**
+ *
+ */
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_iconworks.php']['overrideIconOverlay'][]
+		= 'KayStrobach\\Themes\\Hook\\IconUtilityHook->overrideIconOverlay';
