a432276d8c493c3277a649afef383ef27ec9dd1a
Kay Strobach <github@kay-strobach.de>
2014-03-15 14:59:17 +0100

    First steps with new Constant Editor in THEMES

---
+++ Resources/Public/JavaScript/BackendModule.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+	jQuery('.t3-icon-document-open').click(function() {
+		jQuery(this).parents('tr').removeClass('defaultValueIsSelected');
+		jQuery(this).parents('tr').find('input[type=checkbox]').prop('checked', 'checked');
+	});
+	jQuery('.t3-icon-edit-delete').click(function() {
+		jQuery(this).parents('tr').addClass('defaultValueIsSelected');
+		jQuery(this).parents('tr').find('input[type="checkbox"]').removeAttr('checked');
+	});
+});
