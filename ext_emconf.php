066a5b2814e6d1b7851a8269dda53c87e0e09a04
Kay Strobach <info@kay-strobach.de>
2014-05-29 23:06:11 +0200

    add static_info_tables as dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,6 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
 			'typo3' => '6.2.0-6.2.99',
+			'static_info_tables' => '6.1.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
