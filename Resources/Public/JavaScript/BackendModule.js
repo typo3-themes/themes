0cccac75829f08e858fe2b9d724622d636c795e0
Kay Strobach <info@kay-strobach.de>
2014-05-31 11:02:02 +0200

    [TASK] use new edit icons

--- Resources/Public/JavaScript/BackendModule.js
+++ Resources/Public/JavaScript/BackendModule.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function() {
 	// toggle constant editor
-	jQuery('.t3-icon-document-open').click(function() {
+	jQuery('.showEditForm').click(function() {
 		jQuery(this).parents('tr').removeClass('defaultValueIsSelected');
 		jQuery(this).parents('tr').find('input[type="hidden"]').attr('value', 'checked');
 		jQuery(this).parents('tr').find('input[type="hidden"]').removeAttr('disabled');
 	});
-	jQuery('.t3-icon-edit-delete').click(function() {
+	jQuery('.hideEditForm').click(function() {
 		jQuery(this).parents('tr').addClass('defaultValueIsSelected');
 		jQuery(this).parents('tr').find('input[type="hidden"]').attr('value', '');
 		jQuery(this).parents('tr').find('input[type="hidden"]').attr('disabled','disabled');
