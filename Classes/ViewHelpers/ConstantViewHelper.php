5c3c77b9ec1d1d7c5e7e06298280d665c9965335
Thomas Deuling <info@coding.ms>
2014-06-18 13:32:25 +0200

    Add constant-viewhelper

---
+++ Classes/ViewHelpers/ConstantViewHelper.php
@@ -0,0 +1,31 @@
+<?php
+namespace KayStrobach\Themes\ViewHelpers;
+
+/**
+ * Access constants
+ *
+ * @author Thomas Deuling <typo3@coding.ms>
+ * @package themes
+ */
+class ConstantViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * Gets a constant
+	 *
+     * @param string $constant The name of the constant
+	 * @return string Constant-Value
+     *
+     * = Examples =
+     *
+     * <code title="Example">
+     * <theme:constant constant="themes.configuration.baseurl" />
+     * </code>
+     * <output>
+     * http://yourdomain.tld/
+     * (depending on your domain)
+     * </output>
+	 */
+	public function render($constant='') {
+		return isset($GLOBALS['TSFE']->tmpl->flatSetup[$constant]) ? $GLOBALS['TSFE']->tmpl->flatSetup[$constant] : NULL;
+	}
+}
