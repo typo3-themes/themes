b56cc036974a0a0a9b51aa127b518854abc7b5d2
Kay Strobach <info@kay-strobach.de>
2014-07-02 11:36:32 +0200

    Code Cleanup Configuration / Respect Independent mode

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,43 +9,70 @@ if (!defined('TYPO3_MODE'))
 /**
  * Register hook to inject themes
  */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Tx_Themes_Domain_Repository_ThemeRepository']['init'][] = 'KayStrobach\\Themes\\Hook\\ThemesDomainRepositoryThemeRepositoryInitHook->init';
-
-/**
- * Register a page not found handler, overwrites the one in the install tool
- */
-# @todo refine that in the next version including the standalone mode!
-# $GLOBALS['TYPO3_CONF_VARS']['FE']['pageUnavailable_handling'] = 'USER_FUNCTION:KayStrobach\\Themes\\Hook\\PageNotFoundHandlingHook->main';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Tx_Themes_Domain_Repository_ThemeRepository']['init'][]
+		= 'KayStrobach\\Themes\\Hook\\ThemesDomainRepositoryThemeRepositoryInitHook->init';
 
 /**
  * register used hooks to inject the TS
  */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][] = 'KayStrobach\\Themes\\Hook\\T3libTstemplateIncludeStaticTypoScriptSourcesAtEndHook->main';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][]
+		= 'KayStrobach\\Themes\\Hook\\T3libTstemplateIncludeStaticTypoScriptSourcesAtEndHook->main';
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['moduleBodyPostProcess'][] = 'KayStrobach\\Themes\\Hook\\TemplateModuleBodyPostProcessHook->main';
+/**
+ * register hook to manipulate the template module
+ */
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['moduleBodyPostProcess'][]
+		= 'KayStrobach\\Themes\\Hook\\TemplateModuleBodyPostProcessHook->main';
 
-// Requires signal slot call from http://forge.typo3.org/issues/59703
-/** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
-$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
-$signalSlotDispatcher->connect('TYPO3\\CMS\\Backend\\Utility\\BackendUtility', 'getPagesTSconfigPreInclude', 'KayStrobach\\Themes\\Slots\\BackendUtilitySlot', 'getPagesTSconfigPreInclude');
-unset($signalSlotDispatcher);
+/**
+ * register signal to inject pagets without xclassing
+ * Requires signal slot call from http://forge.typo3.org/issues/59703
+ * @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher
+ */
+	$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
+	$signalSlotDispatcher->connect(
+		'TYPO3\\CMS\\Backend\\Utility\\BackendUtility',
+		'getPagesTSconfigPreInclude',
+		'KayStrobach\\Themes\\Slots\\BackendUtilitySlot',
+		'getPagesTSconfigPreInclude'
+	);
+	unset($signalSlotDispatcher);
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_iconworks.php']['overrideIconOverlay'][] = 'KayStrobach\\Themes\\Hook\\IconUtilityHook';
+/**
+ * register hook for icon overlay
+ */
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_iconworks.php']['overrideIconOverlay'][]
+		= 'KayStrobach\\Themes\\Hook\\IconUtilityHook';
 
-ExtensionUtility::configurePlugin(
-	'KayStrobach.' . $_EXTKEY,
-	'Theme', array(
-		'Theme' => 'index'
-	),
-	array()
-);
+/**
+ * register frontend plugin to allow usage of extbase controller
+ */
+	ExtensionUtility::configurePlugin(
+		'KayStrobach.' . $_EXTKEY,
+		'Theme', array(
+			'Theme' => 'index'
+		),
+		array()
+	);
 
 /**
  * register cache for extension
  */
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache'])) {
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache'] = array();
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache']['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache']['backend'] = 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend';
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache']['options']['compression'] = 1;
-}
+	if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache'])) {
+		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache'] = array();
+		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache']['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
+		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache']['backend'] = 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend';
+		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['themes_cache']['options']['compression'] = 1;
+	}
+
+/**
+ * include stuff for independent mode
+ */
+	$extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['themes']);
+	if(array_key_exists('themesIndependent', $extensionConfiguration) && ($extensionConfiguration['themesIndependent'] === '1')) {
+		/**
+		 * Register a page not found handler, overwrites the one in the install tool
+		 */
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['pageUnavailable_handling'] = 'USER_FUNCTION:KayStrobach\\Themes\\Hook\\PageNotFoundHandlingHook->main';
+	}
+	unset($extensionConfiguration);
--- ext_tables.php
+++ ext_tables.php
@@ -7,59 +7,67 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 if (!defined('TYPO3_MODE'))
 	die('Access denied.');
 
-// Define the TCA for the access control calendar selector.
-$tempColumn = array(
-	'tx_themes_skin' => array(
-		'exclude' => 1,
-		'label' => 'Themes',
-		'displayCond' => 'FIELD:root:REQ:true',
-		'config' => array(
-			'type' => 'user',
-			'userFunc' => 'KayStrobach\\Themes\\Tca\\ThemeSelector->display',
-		)
-	),
-);
-
-if (TYPO3_MODE === 'BE') {
-	ExtensionUtility::registerModule(
-		'KayStrobach.' . $_EXTKEY, 'web', // Main area
-		'mod1', // Name of the module
-		'', // Position of the module
-		array(// Allowed controller action combinations
-			'Editor' => 'index,update,showTheme,setTheme',
-		), array(// Additional configuration
-			'access' => 'user,group',
-			'icon' => 'EXT:themes/ext_icon.png',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml',
-		)
+/**
+ * manipulate the sys_template table
+ */
+	$tempColumn = array(
+		'tx_themes_skin' => array(
+			'exclude' => 1,
+			'label' => 'Themes',
+			'displayCond' => 'FIELD:root:REQ:true',
+			'config' => array(
+				'type' => 'user',
+				'userFunc' => 'KayStrobach\\Themes\\Tca\\ThemeSelector->display',
+			)
+		),
 	);
-}
 
-// Add the skin selector for backend users.
-GeneralUtility::loadTCA('sys_template');
-ExtensionManagementUtility::addTCAcolumns('sys_template', $tempColumn);
-ExtensionManagementUtility::addToAllTCAtypes('sys_template', '--div--;Themes,tx_themes_skin');
+	if (TYPO3_MODE === 'BE') {
+		ExtensionUtility::registerModule(
+			'KayStrobach.' . $_EXTKEY, 'web', // Main area
+			'mod1', // Name of the module
+			'', // Position of the module
+			array(// Allowed controller action combinations
+				'Editor' => 'index,update,showTheme,setTheme',
+			), array(// Additional configuration
+				'access' => 'user,group',
+				'icon' => 'EXT:themes/ext_icon.png',
+				'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml',
+			)
+		);
+	}
 
-// @todo respect independent switch
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'themes');
+	// Add the skin selector for backend users.
+	GeneralUtility::loadTCA('sys_template');
+	ExtensionManagementUtility::addTCAcolumns('sys_template', $tempColumn);
+	ExtensionManagementUtility::addToAllTCAtypes('sys_template', '--div--;Themes,tx_themes_skin');
 
-ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:themes/Configuration/TypoScript/setup.txt">');
-ExtensionManagementUtility::addTypoScriptConstants('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:themes/Configuration/TypoScript/constants.txt">');
+/**
+ * auto inject base TS
+ */
+	$extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['themes']);
+	if(array_key_exists('themesIndependent', $extensionConfiguration) && ($extensionConfiguration['themesIndependent'] === '1')) {
+		ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:themes/Configuration/TypoScript/setup.txt">');
+		ExtensionManagementUtility::addTypoScriptConstants('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:themes/Configuration/TypoScript/constants.txt">');
+	} else {
+		ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'themes');
+	}
+	unset($extensionConfiguration);
 
 /**
  * add themes overlay
  */
-array_push($GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayPriorities'], 'themefound');
-$GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayNames']['themefound'] = 'extensions-themes-overlay-theme';
+	array_push($GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayPriorities'], 'themefound');
+	$GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayNames']['themefound'] = 'extensions-themes-overlay-theme';
 
 /**
  * add sprites
  */
-\TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons(
-	array(
-		'switch-off' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/power_orange.png',
-		'switch-disable' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/power_grey.png',
-		'switch-on' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/power_green.png',
-		'overlay-theme' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/overlay_theme.png',
-	), $_EXTKEY
-);
+	\TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons(
+		array(
+			'switch-off' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/power_orange.png',
+			'switch-disable' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/power_grey.png',
+			'switch-on' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/power_green.png',
+			'overlay-theme' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/overlay_theme.png',
+		), $_EXTKEY
+	);
