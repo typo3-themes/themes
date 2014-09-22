456897980a1af82ae6ed8a256dd2827b7996712f
Kay Strobach <info@kay-strobach.de>
2014-09-22 15:22:36 +0200

    [FEATURE] Sorting of constants and categories Resolves #47

---
+++ Classes/ViewHelpers/Iterator/SortViewHelper.php
@@ -0,0 +1,43 @@
+<?php
+namespace KayStrobach\Themes\ViewHelpers\Iterator;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
+
+
+class SortViewHelper extends AbstractViewHelper {
+
+	/**
+	 * @param array $subject
+	 * @param string $key
+	 * @return array|null
+	 */
+	public function render($subject = NULL, $key = 'label') {
+		$this->arguments['key'] = $key;
+		if (NULL === $subject) {
+			$subject = $this->renderChildren();
+		}
+		$sorted = NULL;
+		if (TRUE === is_array($subject)) {
+			$sorted = $this->sortArray($subject);
+		}
+		return $sorted;
+	}
+
+	/**
+	 * Sort an array
+	 *
+	 * @param array $array
+	 * @return array
+	 */
+	protected function sortArray($array) {
+		usort($array, array($this, 'compare'));
+		return $array;
+	}
+
+
+	public function compare($a, $b) {
+		return strcasecmp($a[$this->arguments['key']], $b[$this->arguments['key']]);
+	}
+
+}
