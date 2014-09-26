3a9ca6d8912fa55ab49f0511229b3ee4e4f202e3
Kay Strobach <info@kay-strobach.de>
2014-09-26 17:34:13 +0200

    [TASK] Layout Theme Selector

---
+++ Classes/ViewHelpers/ThemeEnabledConditionViewHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace KayStrobach\Themes\ViewHelpers;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Access constants
+ *
+ * @author Thomas Deuling <typo3@coding.ms>
+ * @package themes
+ */
+class ThemeEnabledConditionViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper {
+	/**
+	 * @var \KayStrobach\Themes\Domain\Repository\ThemeRepository
+	 * @inject
+	 */
+	protected $themeRepository;
+
+	/**
+	 * @param string $theme
+	 * @return string
+	 * @throws \TYPO3\CMS\Extbase\Mvc\Exception\NoSuchArgumentException
+	 */
+	public function render($theme) {
+		$pageId = intval(GeneralUtility::_GET('id'));
+		$themeOfPage = $this->themeRepository->findByPageOrRootline($pageId);
+
+		if(($themeOfPage !== NULL) && ($themeOfPage->getExtensionName() === $theme)) {
+			return $this->renderThenChild();
+		}
+		return $this->renderElseChild();
+ 	}
+}
