3bfd422cd6d4208ba25fcef19b2a1f7bfba37ac2
Jo Hasenau <jo.hasenau@external.telekom.de>
2014-09-30 10:21:30 +0200

    Modified class mapper to deal with references

--- Classes/Frontend/CssClassMapper.php
+++ Classes/Frontend/CssClassMapper.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace KayStrobach\Themes\Frontend;
+use TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser;
 use TYPO3\CMS\Core\Utility\DebugUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -13,7 +14,15 @@ class CssClassMapper {
 
 	public function mapGenericToFramework($content, $conf) {
 		$genericClasses = array_flip(GeneralUtility::trimExplode(',', $content));
-		$frameworkClasses = $conf['classMapping.'];
+		foreach($conf as $checkConfKey => $checkConfValue) {
+			if(is_array($conf[$checkConfValue])) {
+				continue;
+			} else if ($checkConfValue && strpos($checkConfValue, '<') === 0) {
+				$checkConfValue = ltrim($checkConfValue, '< lib.') . '.';
+				$conf[$checkConfKey] = $GLOBALS['TSFE']->tmpl->setup['lib.'][$checkConfValue];
+			}
+		}
+		$frameworkClasses = array_merge($conf['allClassMapping'], $conf['behaviorClassMapping'], $conf['rowClassMapping'], $conf['columnClassMapping']);
 		$mappedClasses = array_intersect_key($frameworkClasses, $genericClasses);
 		return implode(' ', $mappedClasses);
 	}
