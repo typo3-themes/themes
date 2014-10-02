d5b9bb20e260f922938451a679ecbd8541db3b26
Jo Hasenau <jo.hasenau@external.telekom.de>
2014-10-02 14:00:44 +0200

    Added caching and fixed a small bug with non referenced TypoScript

--- Classes/Frontend/CssClassMapper.php
+++ Classes/Frontend/CssClassMapper.php
@@ -1,33 +1,41 @@
 <?php
 
 namespace KayStrobach\Themes\Frontend;
-use TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser;
-use TYPO3\CMS\Core\Utility\DebugUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
- * Class EditorController
+ * Class CssClassMapper
  *
  * @package KayStrobach\Themes\Frontend
  */
 class CssClassMapper {
 
-	public function mapGenericToFramework($content, $conf) {
-		$frameworkClasses = array();
-		$genericClasses = array_flip(GeneralUtility::trimExplode(',', $content));
-		foreach($conf as $checkConfKey => $checkConfValue) {
-			if(is_array($conf[$checkConfValue])) {
-				continue;
-			} else if ($checkConfValue && strpos($checkConfValue, '<') === 0) {
-				$checkConfValue = ltrim($checkConfValue, '< lib.') . '.';
-				$conf[$checkConfKey] = $GLOBALS['TSFE']->tmpl->setup['lib.'][$checkConfValue];
+	/**
+	 * Maps generic class names of a record to the official class names of the underlying framework
+	 *
+	 * @param string $content
+	 * @param array $conf
+	 * @return string
+	 */
+	public function mapGenericToFramework($content = '', $conf = array()) {
+		if($content) {
+			$hashKey = md5($content . serialize($conf));
+			if(!isset($GLOBALS['TSFE']->themesCssClassMapperCache[$hashKey])) {
+				$frameworkClasses = array();
+				$genericClasses = array_flip(explode(',', $content));
+				foreach($conf as $checkConfKey => $checkConfValue) {
+					if(!is_array($conf[$checkConfValue]) && $checkConfValue && strpos($checkConfValue, '<') === 0) {
+						$checkConfValue = ltrim($checkConfValue, '< lib.') . '.';
+						$conf[$checkConfKey] = $GLOBALS['TSFE']->tmpl->setup['lib.'][$checkConfValue];
+					}
+					if(is_array($conf[$checkConfKey])) {
+						$frameworkClasses = array_merge($frameworkClasses, $conf[$checkConfKey]);
+					}
+				}
+				$mappedClasses = array_intersect_key($frameworkClasses, $genericClasses);
+				$GLOBALS['TSFE']->themesCssClassMapperCache[$hashKey] = implode(' ', $mappedClasses);
 			}
-			if(is_array($conf[$checkConfKey])) {
-				$frameworkClasses = array_merge($frameworkClasses, $conf[$checkConfKey]);
-			}
+			return $GLOBALS['TSFE']->themesCssClassMapperCache[$hashKey];
 		}
-		$mappedClasses = array_intersect_key($frameworkClasses, $genericClasses);
-		return implode(' ', $mappedClasses);
 	}
 
 }
