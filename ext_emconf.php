ddef14a2bed1c7e841da326000e3b1c4a9fe22bf
Thomas Deuling <info@coding.ms>
2018-01-12 18:41:00 +0100

    [TASK] Raise static_info_tables version.

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints'        => [
         'depends' => [
             'typo3'              => '8.7.0-8.7.99',
-            'static_info_tables' => '6.4.0-6.4.99',
+            'static_info_tables' => '6.4.0-6.5.99',
             'gridelements' => '8.0.0-8.7.99',
         ],
         'conflicts' => [
