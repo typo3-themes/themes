441fbc0739d7e88156bb6fe9f2c70287fb7dfbe5
Achim Fritz <achim.fritz@b13.de>
2021-07-01 14:16:03 +0200

    [TASK] add comment to catch statemen

--- Classes/Utilities/CheckPageUtility.php
+++ Classes/Utilities/CheckPageUtility.php
@@ -101,6 +101,7 @@ class CheckPageUtility
                 $themeableSysTemplateRecordsRequireRootFlag = (bool)$configuration['themeableSysTemplateRecordsRequireRootFlag'];
             }
         } catch (SiteNotFoundException $e) {
+            // nothing todo if no site is found
         }
         if ($themeableSysTemplateRecordsRequireRootFlag === true) {
             $queryBuilder->andWhere(
