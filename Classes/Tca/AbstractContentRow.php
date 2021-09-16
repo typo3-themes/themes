35624afaa9b547712212ecedb60c0e5c33fb3edf
Kay Strobach <kaystrobach@users.noreply.github.com>
2021-09-16 06:22:27 +0000

    Apply fixes from StyleCI

--- Classes/Tca/AbstractContentRow.php
+++ Classes/Tca/AbstractContentRow.php
@@ -61,7 +61,8 @@ abstract class AbstractContentRow
             ->from('tt_content')
             ->where(
                 $queryBuilder->expr()->eq(
-                    'uid', $queryBuilder->createNamedParameter(-((int) $pid), \PDO::PARAM_INT)
+                    'uid',
+                    $queryBuilder->createNamedParameter(-((int) $pid), \PDO::PARAM_INT)
                 )
             );
         /** @var  \Doctrine\DBAL\Driver\Statement $statement */
